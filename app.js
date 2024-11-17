be712cf4dbaff92bdba6a8c3097f0cf0d6814dd8
kadir-zeren <k4dzeren@gmail.com>
2024-11-17 12:01:13 +0300

    geliriniz

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
 //? Selectors
+const ekleBtn = document.getElementById("ekle-btn");
+const gelirInput = document.getElementById("gelir-input");
+const ekleFormu = document.getElementById("ekle-formu");
+
+//? Sonuc tablosu
+const gelirinizTd = document.getElementById("geliriniz");
+
 //? Variables
+let gelirler = 0;
+
 //? Events
+
+//? Formun submit butonuna basildiginda
+ekleFormu.addEventListener("submit", (e) => {
+  e.preventDefault(); //? reload'u engeller
+  gelirler = gelirler + Number(gelirInput.value); //? string eklemeyi engelledik
+
+  //? input değerini sifrladik
+  ekleFormu.reset();
+  //   console.log(gelirler);
+
+  //? Değişiklikleri sonuc tablosuna yazan fonks.
+  hesaplaVeGuncelle();
+});
+
 //? Functions
+
+const hesaplaVeGuncelle = () => {
+  gelirinizTd.innerText = gelirler;
+};
